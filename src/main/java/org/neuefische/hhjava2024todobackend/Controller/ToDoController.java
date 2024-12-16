acb13baa92ddab886a95f7193d7d4ab96bac97c5
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-12-16 14:26:10 +0100

    init comit

---
+++ src/main/java/org/neuefische/hhjava2024todobackend/Controller/ToDoController.java
@@ -0,0 +1,9 @@
+package org.neuefische.hhjava2024todobackend.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todo")
+public class ToDoController {
+}
---
+++ src/main/java/org/neuefische/hhjava2024todobackend/HhJava2024ToDoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.neuefische.hhjava2024todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava2024ToDoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava2024ToDoBackendApplication.class, args);
+    }
+
+}
